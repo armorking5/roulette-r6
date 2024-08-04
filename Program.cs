d5150ce4f8c77d9010f3ce95685a431a2a9ce221
Stefano Africano <stefano.africano@outlook.com>
2024-08-04 13:01:40 +0200

    Little Project is Finished

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Logging.AddConfiguration(
 );
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
